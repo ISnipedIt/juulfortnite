8d70ec8aabf3c533a5c52001825160d793d1759c
ISnipedIt <140213675+ISnipedIt@users.noreply.github.com>
2025-05-05 02:47:53 +0000

    Pending changes exported from your codespace

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage.jsx';
+import Download from './components/Download.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
